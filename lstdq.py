2d2a4f6081e94be69af5101499f9d10283aea619
FengH2020 <apisflorea2015@gmail.com>
2021-10-15 21:10:57 +0800

    all files

--- lstdq.py
+++ lstdq.py
@@ -6,6 +6,7 @@
 # @File    : lstdq.py
 # @Software: PyCharm
 
+from copy import copy
 import numpy as np
 from basisfunc import basis_phi
 
@@ -15,7 +16,7 @@ def LSTDQ(sample_mat, degree_, num_action, discount_factor, init_policy):
     # phi_s_a = basis_phi(sample_mat[0, 0], sample_mat[0, 1], basis_dim)
     k = (degree_+1) * num_action
     a_mat = np.zeros((k, k))
-    np.fill_diagonal(a_mat, 0.0001)
+    np.fill_diagonal(a_mat, 0.00001)
     b_vec = np.zeros((k, 1))
 
     for i in range(n):
@@ -23,14 +24,14 @@ def LSTDQ(sample_mat, degree_, num_action, discount_factor, init_policy):
                         np.subtract(
                             basis_phi(sample_mat[i, 0], sample_mat[i, 1], degree_, num_action),
                             discount_factor *
-                            basis_phi(sample_mat[i, 3], init_policy[int(sample_mat[i, 3])], degree_, num_action)
+                            basis_phi(sample_mat[i, 3], init_policy[sample_mat[i, 3]], degree_, num_action)
                         ))
         b_vec += sample_mat[i, 2] * basis_phi(sample_mat[i, 0], sample_mat[i, 1], degree_, num_action).T
 
     a_rank = np.linalg.matrix_rank(a_mat)
     if a_rank == k:
         w = np.linalg.solve(a_mat, b_vec)
+        return w
     else:
         w = np.linalg.lstsq(a_mat, b_vec)
-        raise ValueError("A matrix is not full rank")
-    return w
+        raise ValueError("A matrix {} is not full rank; its lstsq solution is {}".format(a_mat, w))
